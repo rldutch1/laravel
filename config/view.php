fb2346d69723f58f8c68d1dafed8dedb7ab70d65
Robert Holland <rob@robholland.com>
2017-05-14 01:14:55 -0700

    Lesson 6

--- config/view.php
+++ config/view.php
@@ -14,7 +14,7 @@ return [
     */
 
     'paths' => [
-        resource_path('views'),
+        resource_path('views'), //Look in the views folder.
     ],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,32 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+		    return view('welcome');
 });
+
+Route::get('users', function () {
+		$users = [
+		'0' => [
+			'first_name' => 'Robert',
+			'last_name' => 'Holland',
+			'location' => 'USA'
+		],
+		'1' => [
+			'firstname' => 'Leah',
+			'lastname' => 'Holland',
+			'location' => 'USA'
+		],
+		'2' => [
+			'firstname' => 'Jason',
+			'lastname' => 'Holland',
+			'location' => 'USA'
+		],
+		'3' => [
+			'firstname' => 'Keira',
+			'lastname' => 'Holland',
+			'location' => 'USA'
+		]
+	];
+	return $users;
+}
+);
